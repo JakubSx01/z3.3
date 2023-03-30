8a85e81161d0426fb809fd29b714a5ebf9406b08
JakubSx01 <126695976+JakubSx01@users.noreply.github.com>
2023-03-30 13:26:54 +0000

    dziala

--- Main.java
+++ Main.java
@@ -1,16 +1,22 @@
 import java.util.*;
 class Main {
   public static void main(String[] args) {
-    int max=50;
-      int min=-50;
-    int range=max-min+1;
-    int rand = (int)(Math.random() * range) + min;
-     int k=10;
-    char znak='#';
-for(int i=k;i>=0;i--){
-      for(int j=0; j<=k;j++)
-          System.out.print(rand);
+    Random rand = new Random();
+    ArrayList<Integer> liczby = new ArrayList<Integer>();
+    for(int i=0; i<100;i++){
+      int numer = rand.nextInt(101)-50;
+      liczby.add(numer);
+    }
+    System.out.println("Wylosowane liczby:");
+    int l=0;
+for(int k=10;k>0;k--){
+  
+      for(int j=0; j<10;j++){
+          System.out.print(liczby.get(l)+" ");
+        l++;}
           System.out.println();
+          
+
 }
   }
 }
